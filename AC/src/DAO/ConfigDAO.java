8a77d709f723151b4cc64bbdf6d1fd614e32e0cb
xiajibaqu <ljhknight@yeah.net>
2019-01-03 09:08:33 +0800

    Update ConfigDAO.java

--- AC/src/DAO/ConfigDAO.java
+++ AC/src/DAO/ConfigDAO.java
@@ -1,5 +1,4 @@
 package DAO;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,34 +6,24 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
 import entity.Config;
 import util.DBUtil;
-
-public class ConfigDAO {
-	
+public class ConfigDAO {	
 	public int getTotal() {
         int total = 0;
         try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
- 
             String sql = "select count(*) from config";
- 
             ResultSet rs = s.executeQuery(sql);
             while (rs.next()) {
                 total = rs.getInt(1);
             }
- 
             System.out.println("total:" + total);
- 
-        } catch (SQLException e) {
- 
+        } catch (SQLException e) { 
             e.printStackTrace();
         }
         return total;
-    }
- 
+    } 
     public void add(Config config) {
- 
         String sql = "insert into config values(null,?,?)";
         try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
             ps.setString(1, config.key);
@@ -50,48 +39,30 @@ public class ConfigDAO {
             e.printStackTrace();
         }
     }
- 
     public void update(Config config) {
- 
         String sql = "update config set key_= ?, value=? where id = ?";
         try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
- 
             ps.setString(1, config.key);
             ps.setString(2, config.value);
             ps.setInt(3, config.id);
- 
             ps.execute();
- 
         } catch (SQLException e) {
- 
             e.printStackTrace();
         }
- 
     }
- 
     public void delete(int id) {
- 
-        try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
- 
+       try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
             String sql = "delete from config where id = " + id;
- 
             s.execute(sql);
- 
         } catch (SQLException e) {
- 
             e.printStackTrace();
         }
     }
- 
     public Config get(int id) {
         Config config = null;
- 
         try (Connection c = DBUtil.getConnection(); Statement s = c.createStatement();) {
- 
             String sql = "select * from config where id = " + id;
- 
             ResultSet rs = s.executeQuery(sql);
- 
             if (rs.next()) {
                 config = new Config();
                 String key = rs.getString("key_");
@@ -100,30 +71,21 @@ public class ConfigDAO {
                 config.value = value;
                 config.id = id;
             }
- 
         } catch (SQLException e) {
- 
             e.printStackTrace();
         }
         return config;
     }
- 
     public List<Config> list() {
         return list(0, Short.MAX_VALUE);
     }
- 
     public List<Config> list(int start, int count) {
         List<Config> configs = new ArrayList<Config>();
- 
-        String sql = "select * from config order by id desc limit ?,? ";
- 
-        try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) {
- 
+        String sql = "select * from config order by id desc limit ?,? "; 
+        try (Connection c = DBUtil.getConnection(); PreparedStatement ps = c.prepareStatement(sql);) { 
             ps.setInt(1, start);
             ps.setInt(2, count);
- 
             ResultSet rs = ps.executeQuery();
- 
             while (rs.next()) {
                 Config config = new Config();
                 int id = rs.getInt(1);
@@ -135,19 +97,16 @@ public class ConfigDAO {
                 configs.add(config);
             }
         } catch (SQLException e) {
- 
             e.printStackTrace();
         }
         return configs;
     }
- 
     public Config getByKey(String key) {
         Config config = null;
         String sql = "select * from config where key_ = ?" ;
         try (Connection c = DBUtil.getConnection(); 
                 PreparedStatement ps = c.prepareStatement(sql);
-            ) {
-             
+            ) {           
             ps.setString(1, key);
             ResultSet rs =ps.executeQuery();
  
@@ -159,7 +118,6 @@ public class ConfigDAO {
                 config.value = value;
                 config.id = id;
             }
- 
         } catch (SQLException e) {
  
             e.printStackTrace();
